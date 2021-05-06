57e5cac036c04480b2aca44ba4700f62268d47b8
Jesse Willig <jessewillig@outlook.com>
2021-05-06 16:17:38 -0700

    altered component for mobile nav

--- src/components/MobileNav/index.js
+++ src/components/MobileNav/index.js
@@ -4,19 +4,16 @@ import {Link} from "react-router-dom";
 
 function MobileNav() {
     return (
-        <div>
-        <nav id="menu-canvas" uk-offcanvas="overlay: true">
+        <div id="menu-canvas" uk-offcanvas="overlay: true">
             <div className="uk-offcanvas-bar">
                 <button className="uk-offcanvas-close" type="button" uk-close="true"></button>
-                <ul className="uk-list uk-hidden@m">
+                <ul className="uk-list">
                     <li><Link to="/about" className="nav-item mobile-nav-item">About</Link></li>
                     <li><Link to="/portfolio" className="nav-item mobile-nav-item">Portfolio</Link></li>
                     <li><Link to="/skills" className="nav-item mobile-nav-item">Skills</Link></li>
                     <li><Link to="/contact" className="nav-item mobile-nav-item">Contact</Link></li>
                 </ul>
-                <a className="uk-visible@m" href="#menu-canvas" uk-toggle="target: #menu-canvas"><span className="menu-icon" uk-icon="icanon: menu; ratio: 1.5"></span></a>
             </div>
-        </nav>
         </div>
     )
 };
