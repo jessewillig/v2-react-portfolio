582ef256ed94d6c184a93964095ceb9d6a083b63
Jesse Willig <jessewillig@outlook.com>
2021-05-06 20:05:45 -0700

    completed app.js file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
-import './index.css';
-import About from './pages/About';
-// need to import everything within router, meaning all the components
+import React from "react";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import "./index.css";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Skills from "./pages/Skills";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-<Router>
-  <Route exact path='/' component={About} />
-  {/* <Route exact path='/' component={ } />
-  <Route exact path='/' component={ } />
-  <Route exact path='/' component={ } />
-  <Route exact path='/' component={ } /> */}
-  {/* <MobileNav/> */}
-</Router>
+    <Router>
+      <Route exact path='/' component={About} />
+      <Route path="/about" component={About} />
+      <Route path="/portfolio" component={Portfolio} />
+      <Route exact path="/skills" component={Skills} />
+      <MobileMenu />
+      <Footer />
+    </Router>
   )
 }
 
