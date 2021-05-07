3e925c989bed1602c677cc6be65ddc3b1a0582f9
Jesse Willig <jessewillig@outlook.com>
2021-05-06 17:09:58 -0700

    added skill card component

--- src/components/SkillCard/index.js
+++ src/components/SkillCard/index.js
@@ -3,7 +3,21 @@ import './style.css';
 
 function Nav() {
     return (
-
+        <div className="uk-text-center client-side-card">
+            <div className={`uk-card uk-card-default ${props.name}-card ${props.animate}`}>
+                <div className="uk-card-body">
+                    <h3 className="uk-card-title">{props.title}</h3>
+                    <ul className="uk-list uk-list-divider">
+                        {props.skills.map(skill => <li>{skill}</li>)}
+                    </ul>
+                </div>
+                <div className={`uk-card-footer portfolio-card-footer ${props.name}-card-footer`}>
+                    <div>
+                        <span className="skills-icon" uk-icon={`icon: ${props.icon}; ratio: 1.5`}></span>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 };
 
